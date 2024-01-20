9ad696256667b998232d84fdd717cc42c4062b2d
Qluqlu3 <plksmcgof@gmail.com>
2024-01-20 16:03:01 +0900

    fix iconlink

--- .build/static/chunks/webpack.js
+++ .build/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f0e7a19eb4694a4"; }
+/******/ 		__webpack_require__.h = function() { return "97f7acbd294faf95"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/components/Layouts/Menu.tsx
+++ src/app/components/Layouts/Menu.tsx
@@ -5,13 +5,10 @@ import { IconLink } from '../IconLink';
 const Menu: React.FC = () => {
   return (
     <div className='flex flex-row gap-x-3 items-center'>
+      <IconLink icon={<MdHome className='w-16 h-16' />}></IconLink>
       <IconLink icon={<MdComment className='w-16 h-16' />} href='/setting'></IconLink>
       <IconLink icon={<MdEqualizer className='w-16 h-16' />} href='/setting'></IconLink>
       <IconLink icon={<MdSettings className='w-16 h-16' />} href='/setting'></IconLink>
-      {/* <MdSettings className='w-16 h-16 p-2 hover:bg-green-500 rounded-2xl cursor-pointer' /> */}
-      {/* <div className='w-16 h-16'> */}
-      <IconLink icon={<MdHome className='w-16 h-16' />}></IconLink>
-      {/* </div> */}
     </div>
   );
 };
