3bd6a9a5f9bc3cdffd2e69d4ff320252d4700535
Qluqlu3 <plksmcgof@gmail.com>
2024-01-20 15:02:29 +0900

    fix

--- .build/static/chunks/webpack.js
+++ .build/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "434a434d12152fe2"; }
+/******/ 		__webpack_require__.h = function() { return "bfdf143386e458e8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .storybook/dist/main.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
-  // ストーリーファイルのパス
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-next']
-};
